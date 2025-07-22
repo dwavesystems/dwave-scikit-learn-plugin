08f3ec2472ffe821be2666971e9fb2dafea1ec38
tducey03 <ted197@msstate.edu>
2025-07-22 15:23:30 -0400

    Remove unnecessary comments and whitespace. Update variables to follow PEP 8

--- dwave/plugins/sklearn/transformers.py
+++ dwave/plugins/sklearn/transformers.py
@@ -55,7 +55,6 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
             The number of features to select.
         time_limit:
             The time limit for the run on the hybrid solver.
-
     """
 
     ACCEPTED_METHODS = [
@@ -155,7 +154,7 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
         # our objective
         # we multiply by 2 because the matrix is symmetric
         np.fill_diagonal(correlations, correlations[:, -1] * (-2 * alpha * num_features))
-    
+
         cqm = dimod.ConstrainedQuadraticModel()
         cqm.add_variables(dimod.BINARY, X.shape[1])
 
@@ -213,21 +212,21 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
         """
 
         # initialize model, create binary list, make constant
-        NL = Model()
+        nl = Model()
         total_num_features=X.shape[1]
-            
-        X_binary = NL.binary(total_num_features) 
-        var_features = NL.constant(num_features)
+
+        x_binary = nl.binary(total_num_features)
+        var_features = nl.constant(num_features)
         feat_corr = correlations[:-1,:-1]
-            
+
         # take last element in every row
         label_corr = np.array(correlations[:-1,-1])
 
         # Make a constant node in order to splice and use in objective
-        NL_corr = NL.constant(feat_corr)
+        nl_corr = nl.constant(feat_corr)
 
         # extract upper triangle, excluding diagonal. Flatten into 1D array
-        C = np.triu(NL_corr, k=1).flatten()
+        C = np.triu(nl_corr, k=1).flatten()
 
         # generate all column and row indices
         quad_col = np.tile(np.arange(total_num_features), total_num_features)
@@ -235,7 +234,7 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
                     (total_num_features,1)).flatten('F')
 
         # extract indices where correlation value not equal to zero
-            
+
         # j index
         q2 = quad_col[C != 0]
         # i index
@@ -249,17 +248,17 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
         expected_linear = np.zeros(len(feat_corr[0]))
 
         # numpy will automatically go element-by-element in the arrays
-        linear += NL.constant(-1.0 * label_corr * alpha * num_features)
+        linear += nl.constant(-1.0 * label_corr * alpha * num_features)
         expected_linear += (-1.0 * label_corr * alpha * num_features)
 
         # if must choose exact number of desired features
         if strict:
-            NL.add_constraint(X_binary.sum() == var_features)
+            nl.add_constraint(x_binary.sum() == var_features)
         else:
-            NL.add_constraint(X_binary.sum() <= var_features)
+            nl.add_constraint(x_binary.sum() <= var_features)
 
-        NL.minimize(NL.constant(2.0) * NL.quadratic_model(X_binary, quadratic=(q3, [q1, q2]), linear=linear))
-        return NL, X_binary, expected_linear
+        nl.minimize(nl.constant(2.0) * nl.quadratic_model(x_binary, quadratic=(q3, [q1, q2]), linear=linear))
+        return nl, x_binary, expected_linear
 
     @staticmethod
     def correlation(
@@ -270,7 +269,7 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
         num_features: int,
         strict: bool = True,
         solver: str,
-    ) -> dimod.ConstrainedQuadraticModel | tuple[Model, list, np.array]: 
+    ) -> dimod.ConstrainedQuadraticModel | tuple[Model, list, np.array]:
         """Build a model for feature selection.
 
         This method is based on maximizing influence and feature independence as
@@ -296,7 +295,7 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
                 If ``False`` the constraint on the number of selected features
                 is ``<=`` rather than ``==``.
             solver:
-                String dictating whether to use the CQM or NL model creation method
+                String containing either "cqm" or "nl" to decide which solver creation method to use. Defaults to "cqm"
 
         Returns:
             A constrained quadratic model
@@ -329,8 +328,6 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
         if X.shape[0] <= 1:
             raise ValueError("X must have at least two rows")
 
-        
-
         with tempfile.TemporaryFile() as fX, tempfile.TemporaryFile() as fout:
             # we make a copy of X because we'll be modifying it in-place within
             # some of the functions
@@ -353,18 +350,12 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
             # the penalty/quality
             np.absolute(correlations, out=correlations)
 
-            # our objective
-
-            # Note: the full symmetric matrix (with both upper- and lower-diagonal
-            # entries for each correlation coefficient) is retained for consistency with
-            # the original formulation from Milne et al.
             if (solver == "cqm"):
                 return SelectFromQuadraticModel._create_cqm_model(correlations=correlations, X=X, alpha=alpha, num_features=num_features, strict=strict)
             elif (solver == "nl"):
                 return SelectFromQuadraticModel._create_nl_model(correlations=correlations, X=X, alpha=alpha, num_features=num_features, strict=strict)
             else:
-                print("Unknown Solver Selected")
-                return -1
+                raise ValueError(f"Solver parameter must be equal to 'nl' or 'cqm'. Received solver parameter: {solver}")
 
     def fit(
         self,
@@ -423,12 +414,12 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
         if num_features >= X.shape[1]:
             self._mask = np.ones(X.shape[1], dtype=bool)
             return self
-        
+
         if self.method == "correlation":
             if solver == "cqm":
                 cqm = self.correlation(X, y, num_features=num_features, alpha=alpha, solver=solver)
             elif solver == "nl":
-                NL, X_binary, _ = self.correlation(X, y, num_features=num_features, alpha=alpha, solver=solver)
+                nl, x_binary, _ = self.correlation(X, y, num_features=num_features, alpha=alpha, solver=solver)
 
         else:
             raise ValueError(f"only methods {self.acceptable_methods} are implemented")
@@ -453,13 +444,13 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
                 sampler = LeapHybridNLSampler()
 
                 # time_limit is checked by the LeapHybridNLSampler
-                sampler.sample(NL, time_limit=self.time_limit, label='NL Plug-IN')
+                sampler.sample(nl, time_limit=self.time_limit, label='scikit-learn Plug-In: NL')
 
                 # Get the index position of chosen features
                 # Example Given (e.g.) of 6 features to choose 3
-                with NL.lock():
-                    selected = X_binary.state(0) # e.g. [0,1,0,0,1,1,0]
-                    NL.unlock()
+                with nl.lock():
+                    selected = x_binary.state(0) # e.g. [0,1,0,0,1,1,0]
+                    nl.unlock()
 
                 mask = np.asarray(selected, dtype=bool) # e.g. [False, True, False, False, True, True, False]
                 self._mask = mask
@@ -472,7 +463,7 @@ class SelectFromQuadraticModel(SelectorMixin, BaseEstimator):
                 access to Leap’s solvers.
                 """
             )
-    
+
         return self
 
     def unfit(self):
